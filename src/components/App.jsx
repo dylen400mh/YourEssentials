baeea29ca7e6d0585677f8fac2445d5dd4690086
Dylen <dylenbelanger2004@gmail.com>
2023-09-30 23:23:24 -0400

    Create cart state variable.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import Header from "./Header";
+
 function App() {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <div>
+      <Header cart={cart} />
+      {/* Other components */}
+    </div>
+  );
 }
 
 export default App;
