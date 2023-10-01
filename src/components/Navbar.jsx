27cdb350998862ae3c4a243cc1d6f2a6beae617e
Dylen <dylenbelanger2004@gmail.com>
2023-10-01 18:54:17 -0400

    Pass data-testid as props to Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function Navbar() {
+function Navbar(props) {
   return (
-    <nav data-testid="Navbar">
+    <nav data-testid={props["data-testid"]}>
       <Link to="/">Home</Link>
       <Link to="shop">Shop</Link>
     </nav>
   );
 }
+
+Navbar.propTypes = {
+  "data-testid": PropTypes.string,
+};
 
 export default Navbar;
