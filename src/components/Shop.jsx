262d82e0b8a5e1c6cc777439f31af69ea69c4da4
Dylen <dylenbelanger2004@gmail.com>
2023-10-28 19:25:12 -0400

    Add error and loading components.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -5,21 +5,31 @@ import { ShopContext } from "./App";
 function Shop() {
   const { cart } = useContext(ShopContext);
 
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  // fetch data from fakestore api
   useEffect(() => {
-    const fetchData = async () => {
-      const response = fetch("https://fakestoreapi.com/products/")
-        .then((res) => res.json())
-        .then((json) => console.log(json));
-      setProducts(response);
-    };
-    fetchData();
+    fetch("https://fakestoreapi.com/products/", {mode: "cors"})
+      .then((res) => {
+        if (res.status >= 400) {
+            throw new Error("server error");
+        }
+
+        return res.json();
+      })
+      .then((json) => setProducts(json))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
+
+
 
   return (
     <div>
       <Header cart={cart}></Header>
+      {loading ? <p>Loading...</p> : error ? <p>An error has occured: {error}</p> : <ShopItems products={products}/>}
     </div>
   );
 }
