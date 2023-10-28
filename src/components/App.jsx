320018478fcfd991997aa9f01c2773b5a0f4bf33
Dylen <dylenbelanger2004@gmail.com>
2023-10-28 14:02:19 -0400

    Add welcome message and shop button to home page.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./Header";
+import { Link } from "react-router-dom";
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -7,7 +8,15 @@ function App() {
   return (
     <div>
       <Header cart={cart} />
-      {/* Other components */}
+      <h1>Welcome to YourEssentials</h1>
+      <p>
+        Your One-Stop Shop for Quality and Convenience! Discover a world of
+        endless possibilities, browse our curated selection of products, and
+        indulge in a seamless shopping experience. From fashion to electronics,
+        home decor to personal care, we&apos;ve got it all. Start exploring now,
+        and let us redefine the way you shop!
+      </p>
+      <Link to="/shop">Shop Now!</Link>
     </div>
   );
 }
