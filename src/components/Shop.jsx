725d2776129563a7df4b82902dbdf1a7c096c639
Dylen <dylenbelanger2004@gmail.com>
2023-10-28 19:04:11 -0400

    Fetch store items from FakeStore API

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,27 @@
 import Header from "./Header";
-import {useContext} from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "./App";
 
 function Shop() {
-    const {cart} = useContext(ShopContext);
+  const { cart } = useContext(ShopContext);
 
-    return <div>
-        <Header cart={cart}></Header>
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = fetch("https://fakestoreapi.com/products/")
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+      setProducts(response);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Header cart={cart}></Header>
     </div>
+  );
 }
 
 export default Shop;
