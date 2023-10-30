9754b6ada4db5f2545cc4fde99edbfda60d4679c
Dylen <dylenbelanger2004@gmail.com>
2023-10-30 11:32:37 -0400

    Modify add and remove functions to properly update cart.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,25 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
-    setCart([...cart, item]);
-    console.log("Added");
+    
+    const oldCount = cart.some((cartItem) => cartItem.id === item.id) ? cart[item].quantity : 0;
+    const newEntry = { item: item, quantity: oldCount + 1 };
+
+    setCart([
+      ...cart.filter((cartItem) => cartItem.item.id !== item.id),
+      newEntry,
+    ]);
   };
 
   const removeFromCart = (item) => {
-    setCart(cart.filter((cartItem) => cartItem !== item));
+    const oldCount = cart[item].quantity;
+    const newEntry = { item: item, quantity: oldCount - 1 };
+    newEntry.quantity
+      ? setCart([
+          ...cart.filter((cartItem) => cartItem.item.id !== item.id),
+          newEntry,
+        ])
+      : setCart(cart.filter((cartItem) => cartItem.item.id !== item.id));
   };
 
   const router = createBrowserRouter([
@@ -28,6 +41,7 @@ function App() {
       element: (
         <Shop
           cart={cart}
+          setCart={setCart}
           addToCart={addToCart}
           removeFromCart={removeFromCart}
         />
