a15220b01ba4e250fc7da9cb9be266e85d6a7056
dylen400mh <dylenbelanger2004@gmail.com>
2023-11-28 10:13:37 -0500

    Add tailwind styling for Home and Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,22 @@ import Navbar from "./Navbar";
 import CartButton from "./CartButton";
 import PropTypes from "prop-types";
 
-function Header({cart}) {
-
+function Header({ cart }) {
   return (
-    <header>
-      <h1>YourEssentials</h1>
-      <Navbar data-testid="Navbar" />
-      <CartButton cart={cart} />
+    <header className="bg-indigo-700 text-white py-4 fixed top-0 w-full">
+      <div className="flex items-center justify-between mx-3">
+        <h1 className="text-2xl font-bold">YourEssentials</h1>
+        <div className="flex items-center space-x-4 gap-2">
+          <Navbar data-testid="Navbar" />
+          <CartButton cart={cart} />
+        </div>
+      </div>
     </header>
   );
 }
 
 Header.propTypes = {
   cart: PropTypes.array,
-}
+};
 
 export default Header;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,26 @@ import PropTypes from "prop-types";
 
 function Home({ cart }) {
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
       <Header cart={cart} />
-      <h1>Welcome to YourEssentials</h1>
-      <p>
-        Your One-Stop Shop for Quality and Convenience! Discover a world of
-        endless possibilities, browse our curated selection of products, and
-        indulge in a seamless shopping experience. From fashion to electronics,
-        home decor to personal care, we&apos;ve got it all. Start exploring now,
-        and let us redefine the way you shop!
-      </p>
-      <Link to="/shop">Shop Now!</Link>
+      <div className="text-center my-8">
+        <h1 className="text-4xl font-bold text-indigo-700 mb-4">
+          Welcome to YourEssentials
+        </h1>
+        <p className="text-gray-600">
+          Your One-Stop Shop for Quality and Convenience! Discover a world of
+          endless possibilities, browse our curated selection of products, and
+          indulge in a seamless shopping experience. From fashion to
+          electronics, home decor to personal care, we&apos;ve got it all. Start
+          exploring now, and let us redefine the way you shop!
+        </p>
+        <Link
+          to="/shop"
+          className="mt-6 inline-block bg-indigo-500 text-white px-4 py-2 rounded-md transition duration-300 hover:bg-indigo-600"
+        >
+          Shop Now!
+        </Link>
+      </div>
     </div>
   );
 }
